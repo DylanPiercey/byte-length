bbda0006b962d3888e95d06be5544e994db74153
Dylan Piercey <pierceydylan@gmail.com>
2015-12-17 21:10:44 -0700

    Use strict mode and fix accidental global

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
  * Calculate the byte lengths for utf8 encoded strings.
  *
@@ -5,10 +7,11 @@
  * @return {Number}
  */
 module.exports = function byteLength (str) {
+  var i, len;
   if (!str) return 0;
   str = str.toString();
 
-  for (var i = len = str.length; i--;) {
+  for (i = len = str.length; i--;) {
     var code = str[i].charCodeAt();
     if (0xDC00 <= code && code <= 0xDFFF) i--;
     if (0x7f < code && code <= 0x7ff) len++;
